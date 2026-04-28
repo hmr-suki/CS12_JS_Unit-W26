91384062176c97120b5e75e0679bc45ed36d7979
hmr-suki <yanan2023715@gmail.com>
2026-04-28 11:25:48 -0300

    222

--- Homework/JS Project - Guessing Game/script.js
+++ Homework/JS Project - Guessing Game/script.js
@@ -7,8 +7,37 @@
 ---Update message and scores etc
 */
 
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min); // maximum and minimum are inclusive
+  }
+let randNum = getRandomIntInclusive(1,20);
+
+let messageObj = document.querySelector(".message");
+let guessObj = document.querySelector('.guess') ;
+let scoreObj = document.querySelector('.score');
+let hscoreObj = document.querySelector('.highscore');
+console.log(scoreObj.textContent)
+console.log(hscoreObj)
 
 
+function guess(){
+    let userGuess = Number(guessObj);
+    if (userGuess == randNum){
+        messageObj = 'You win!';
+        if (hscoreObj < scoreObj){
+            hscoreObj = scoreObj;
+            document.querySelector('.highscore').textContent = hscoreObj
+
+        }
+
+    }else{
+        messageObj = 'Try again';
+        scoreObj = scoreObj -1;
 
 
+    }
 
+
+}
+
+document.querySelector()
