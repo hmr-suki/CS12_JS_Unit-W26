0cd39deee7c6a339b72a676146613fefc2b2347a
hmr-suki <yanan2023715@gmail.com>
2026-05-14 10:12:28 -0300

    11

--- L9 - Forms (Burger Form)/script.js
+++ L9 - Forms (Burger Form)/script.js
@@ -1,3 +1,21 @@
 document.querySelector(".subtotal").value = '$0';
 document.querySelector(".tax").value = '15%';
 document.querySelector(".total").value = '$0';
+
+document.querySelector(".button1").addEventListener("click", order);
+
+function order() {
+    let burgerNum = document.querySelector(".burgerNumber").value;
+    let friesNum = document.querySelector(".friesNumber").value;
+    let drinksNum = document.querySelector(".drinksNumber").value;
+    let subtotal = 6*burgerNum + 3*friesNum + 1.5*drinksNum;
+    let tax= 0.15*subtotal
+    let total = tax + subtotal;
+
+    document.querySelector(".subtotal").value ="$" + subtotal;
+    document.querySelector(".tax").value = "$" + tax;
+    document.querySelector(".total").value ="$"+ total;
+
+    
+
+}
